2523cc762dc51bd415832f576952187ee32d9198
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-17 07:04:05 +0900

    Create zmooth_twrp.mk

---
+++ zmooth_twrp.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a05bd device
+$(call inherit-product, device/gsl/zmooth/device.mk)
+
+PRODUCT_DEVICE := a05bd
+PRODUCT_NAME := omni_a05bd
+PRODUCT_BRAND := benesse
+PRODUCT_MODEL := a05bd
+PRODUCT_MANUFACTURER := sts
+
+PRODUCT_GMS_CLIENTID_BASE := android-sts
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="TAB-A05-BD-user 9 01.00.000 01.00.000 release-keys"
+
+BUILD_FINGERPRINT := benesse/TAB-A05-BD/TAB-A05-BD:9/01.00.000/01.00.000:user/release-keys
