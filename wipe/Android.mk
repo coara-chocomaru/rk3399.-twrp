3b9c3105d90b15789f97346930129159c851d103
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-01-07 18:21:40 +0900

    Update Android.mk

--- wipe/Android.mk
+++ wipe/Android.mk
@@ -1,1 +1,16 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := wipe_helper
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := wipe.c
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := \
+    libc \
+    liblog
+
+LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+include $(BUILD_EXECUTABLE)
